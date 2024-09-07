8ed3fc089fd4b04cdff98a70a52ca0ce555d7ce5
Hector Brayan Velazquez Serrano <hecthor192000@gmail.com>
2024-09-07 10:07:48 -0600

    Primer commit de intro ts

---
+++ src/Temas-ts/01-tipos-bas.ts
@@ -0,0 +1,14 @@
+var num1=23;
+let num2='Juan';
+const num3=34;
+
+const sum = num1 + num2 +num3;
+console.log(`The sum is: ${sum}`)
+
+let nombre:string='Juan';
+let edad:number =23;
+let activo:boolean = true; 
+
+let matricula:number|string;
+matricula=123456;
+matricula='gbu667676b'
